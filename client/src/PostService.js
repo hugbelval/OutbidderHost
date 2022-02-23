c4d8be95e1ace6c52594e3295057588624ea4c13
Gabriel Morris <g4bmorris@gmail.com>
2022-02-22 20:51:27 -0500

    Ajout des fichiers de base

---
+++ client/src/PostService.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+import axios from "axios";
+
+const url = 'encant/';
+
+class PostService {
+    // Get Posts
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            } catch(err){
+                reject(err);
+            }
+        });
+    }
+
+    // Create Post
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        });
+    }
+
+    // Delete post
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`);
+    }
+}
+
+export default PostService;
