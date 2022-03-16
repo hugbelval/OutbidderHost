5af477385089684183feb2889b319f1d34894956
Hugo Bellemare-Vallieres <hugo.bv@videotron.ca>
2022-03-16 11:59:33 -0400

    JWT côté serveur

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 // const mongodb = require('mongodb');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const router = express.Router();
 const User = require('../../models/users');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Get Users
 router.get('/', async (req, res) => {
+    //decodedToken = jwt.verify(token, process.env.SECRET_JWT);
+    
     User.find()
     .then(posts => {
         res.send(posts);
@@ -20,10 +25,10 @@ router.get('/', async (req, res) => {
 
 exports.createPost = (req, res, next) => {
     
-  }
+}
 
 // Add User
-router.post('/', async (req, res) =>{
+router.post('/signup', async (req, res, next) =>{
     console.log("Body: %j", req.body);
     const username = req.body.username;
     const firstname = req.body.firstname;
@@ -32,28 +37,69 @@ router.post('/', async (req, res) =>{
     const email = req.body.email;
     const phone = req.body.phone;
 
-    console.log(username);
-  
-    // Crée un nouvel article avec les informations du formulaire
-    const user = new User({
-      username: username,
-      firstname: firstname,
-      lastname: lastname,
-      password: password,
-      email: email,
-      phone: phone
-    });
-  
-    // Enregistre l'article dans la base de données
-    // Utilisation de la méthode save() qui retourne une promesse
-    user.save()
-      .then( () => {
-        res.status(201).send();
-      })
-      .catch(err => {
-        console.log('err', err);
+    bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+        const user = new User({
+            email: email,
+            password: hashedPassword,
+            username: username,
+            firstname: firstname,
+            lastname: lastname,
+            phone: phone
+        });
+        return user.save();
+    })
+    .then(result => {
+        res.status(201).json({message: "Utilisateur créé !", userId: result.id});
+    })
+    .catch(err => {
+        next(err);
+    })
+});
+
+router.post('/login', async (req, res, next) =>{
+    console.log("Body: %j", req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let loadedUser;
+    User.findOne({email:email})
+    .then(user => {
+        if(!user) {
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        loadedUser = user;
+        return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+        if (!isEqual) {
+            const error = new Error('Wrong password');
+            error.statusCode = 401;
+            throw error;
+        }
+        // Création du token JWT
+        const token = jwt.sign(
+            {
+                email: loadedUser.email,
+                username:loadedUser.username,
+                firstname: loadedUser.firstname,
+                lastname: loadedUser.lastname,
+                phone: loadedUser.phone,
+                userId: loadedUser._id.toString()
+            },
+            `${process.env.JWT_SECRET_KEY}`,
+            { expiresIn: '1h' }
+        );
+        res.status(200).json({token:token});
+    }).catch(err =>{
+        if (!err.statusCode) err.statusCode = 500;
+        res.status(err.statusCode).json({ message: err.message, statusCode: err.statusCode });
       });
 });
+
 
 // Delete User
 router.delete('/:id', async (req, res) => {
